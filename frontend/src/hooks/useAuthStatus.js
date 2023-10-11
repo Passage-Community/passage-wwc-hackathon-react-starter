2bd75fa331142f63cda808d99bc71e720435ba95
Blayne Bayer <blayne.bayer@passage.id>
2023-10-11 13:27:09 -0500

    change backend port to 7001

--- frontend/src/hooks/useAuthStatus.js
+++ frontend/src/hooks/useAuthStatus.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:7000";
+const API_URL = "http://localhost:7001";
 
 export function useAuthStatus() {
   const [result, setResult] = useState({
