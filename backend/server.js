87cb7152d4eef72ee28a21621a9cf49045947439
Franziska S <fra.se@posteo.net>
2023-10-14 20:40:51 +0200

    chore(backend): use eslint, prettier and run it as precommit hook

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const Passage = require("@passageidentity/passage-node");
-const cors = require("cors");
+const express = require('express');
+const Passage = require('@passageidentity/passage-node');
+const cors = require('cors');
 
 const app = express();
 const PORT = 7001;
-const CLIENT_URL = "http://localhost:3000";
+const CLIENT_URL = 'http://localhost:3000';
 
-require("dotenv").config();
+require('dotenv').config();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: CLIENT_URL,
+    origin: CLIENT_URL
   })
 );
 
 const passage = new Passage({
   appID: process.env.PASSAGE_APP_ID,
   apiKey: process.env.PASSAGE_API_KEY,
-  authStrategy: "HEADER",
+  authStrategy: 'HEADER'
 });
 
-app.post("/auth", async (req, res) => {
+app.post('/auth', async (req, res) => {
   try {
     const userID = await passage.authenticateRequest(req);
     if (userID) {
@@ -30,15 +30,15 @@ app.post("/auth", async (req, res) => {
       const identifier = email ? email : phone;
 
       res.json({
-        authStatus: "success",
-        identifier,
+        authStatus: 'success',
+        identifier
       });
     }
   } catch (e) {
     // authentication failed
     console.log(e);
     res.json({
-      authStatus: "failure",
+      authStatus: 'failure'
     });
   }
 });
