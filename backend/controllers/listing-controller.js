7efdb872eeea9c77834bf7c651ff411237a3b662
Mary Rodriguez <maryrodri3210@gmail.com>
2023-10-18 17:49:51 -0500

    minor changes

--- backend/controllers/listing-controller.js
+++ backend/controllers/listing-controller.js
@@ -8,6 +8,30 @@ require("dotenv").config();
 
 
 
+//retrieving listing by radius of location
+// router.get("/radius", async (req, res) => {
+//     try {
+//       console.log("b");
+//       const searchRadius = 8046 //5 miles in radius
+//       db.Listing.createIndex( { zipCoords : "2dsphere" } )
+//       const nearByListing = Listing.find({ zipCoords:
+//         { $near: {
+//             $geometry: [req.body.zipCoords.coordinates[0], req.body.zipCoords.coordinates[1]],
+//             $maxDistance: searchRadius
+//           }
+//         }
+//       });
+//     //   const allListing = await Listing.find({
+
+//     //   });
+//     //   console.log(allListing);
+//     console.log(nearByListing)
+//       res.status(200).json(nearByListing);
+//     } catch (err) {
+//       res.status(400).json({ error: err });
+//     }
+//   });
+
 
 // retrieve information from all listings
 router.get("/", async (req, res) => {
@@ -49,7 +73,6 @@ router.post("/", async (req, res) => {
         fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.GEOCODE_API_KEY}&location=${zipCode}`)
         .then((res) => res.json())
         .then( async (json) => {
-        // setData(json.results);
             const zipInfo = (json.results[0].locations[0].latLng);
             req.body.zipCoords = { type: "Point", coordinates: [zipInfo.lat, zipInfo.lng]};
             const newListing = await Listing.create(req.body);
